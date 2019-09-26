1b2a75fb5b33be78179143a4a8c27a9568c7e1d9
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-26 17:33:35 +0600

    external filter orders by lab of clinician

--- src/components/dentist/Dashboard/CaseOrders.vue
+++ src/components/dentist/Dashboard/CaseOrders.vue
@@ -58,12 +58,10 @@ export default {
     let userType = this._getUserType()
     let options = {}
     if (userType === 'Dentist') {
-      console.log('TCL: mounted -> userType', userType)
       let dentistId = this.currentUserId
       options = { where: ['dentistId', '==', dentistId] }
     }
     this.$store.dispatch('LOAD_ORDERS', options)
-    console.log('TCL: mounted -> options', options)
   }
 }
 </script>
--- src/components/lab/Dashboard/CaseOrderTableFilter.vue
+++ src/components/lab/Dashboard/CaseOrderTableFilter.vue
@@ -2,28 +2,58 @@
     <div class="row">
       <div class="col-md-12">
       <span class="btn-group-toggle ml-2" data-toggle="buttons">
-        <label class="btn btn-secondary">
-          <input type="checkbox" checked autocomplete="off"> All
+        <label
+          class="btn btn-secondary"
+          :class="{'active' : currentFilter==='All'}">
+          <input
+            type="checkbox"
+            checked
+            autocomplete="off"
+            @change="filterTrigger('All')">All
         </label>
       </span>
         <span class="btn-group-toggle ml-2" data-toggle="buttons">
-          <label class="btn btn-secondary active">
-            <input type="checkbox" checked autocomplete="off"> In review
+          <label
+            class="btn btn-secondary"
+            :class="{'active' : currentFilter==='In review'}">
+            <input
+              type="checkbox"
+              checked
+              autocomplete="off"
+              @change="filterTrigger('In review')">In review
           </label>
         </span>
         <span class="btn-group-toggle ml-2" data-toggle="buttons">
-          <label class="btn btn-secondary">
-            <input type="checkbox" checked autocomplete="off"> Assigned to the Dentist
+          <label
+            class="btn btn-secondary"
+            :class="{'active' : currentFilter==='Assigned to the Dentist' }">
+            <input
+              type="checkbox"
+              checked
+              autocomplete="off"
+              @change="filterTrigger('Assigned to the Dentist')">Assigned to the Dentist
           </label>
         </span>
         <span class="btn-group-toggle ml-2" data-toggle="buttons">
-          <label class="btn btn-secondary">
-            <input type="checkbox" checked autocomplete="off"> Assigned to the Lab
+          <label
+            class="btn btn-secondary"
+            :class="{'active' : currentFilter==='Assigned to the Lab'}">
+            <input
+              type="checkbox"
+              checked
+              autocomplete="off"
+              @change="filterTrigger('Assigned to the Lab')">Assigned to the Lab
           </label>
         </span>
         <span class="btn-group-toggle ml-2" data-toggle="buttons">
-          <label class="btn btn-secondary">
-            <input type="checkbox" checked autocomplete="off"> With clinician alert
+          <label
+            class="btn btn-secondary"
+            :class="{'active' : currentFilter==='With clinician alert'}">
+            <input
+              type="checkbox"
+              checked
+              autocomplete="off"
+              @change="filterTrigger('With clinician alert')">With clinician alert
           </label>
         </span>
       </div>
@@ -32,7 +62,40 @@
 
 <script>
 export default {
-  name: 'CaseOrderTableFilter'
+  name: 'CaseOrderTableFilter',
+  data () {
+    return {
+      currentFilter: 'All'
+    }
+  },
+  methods: {
+    filterTrigger (mode) {
+      this.currentFilter = mode
+      let options = this._optionsSetter(mode)
+      this.$store.dispatch('LOAD_ORDERS', options)
+    },
+    _optionsSetter (mode) {
+      let _options
+      switch (mode) {
+        case 'In review':
+          _options = { where: ['status', '==', mode] }
+          break
+        case 'Assigned to the Dentist':
+          _options = { where: ['attention_require', '==', 'Dentist'] }
+          break
+        case 'Assigned to the Lab':
+          _options = { where: ['attention_require', '==', 'Lab Staff'] }
+          break
+        case 'With clinician alert':
+          _options = { where: ['clinical_alert', '==', true] }
+          break
+        default:
+          _options = []
+          break
+      }
+      return _options
+    }
+  }
 }
 </script>
 
