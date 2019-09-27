e4c0b536706be323456de19afb1611a7cb27c5bf
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-27 15:17:34 +0600

    Clinician stats

--- src/components/clinician/Dashboard/Stats.vue
+++ src/components/clinician/Dashboard/Stats.vue
@@ -3,12 +3,12 @@
     <div class="col-md-12">
       <div class="row">
         <div class="col-12 col-md-4">
-          <p>Total Cases: {{clinicianStats.totalCases}}</p>
-          <p>Cases Completed: {{clinicianStats.casesCompleted}}</p>
+          <p>Total Cases: {{ statsTotalCase }}</p>
+          <p>Cases Completed: {{ statsCaseCompleted }}</p>
         </div>
         <div class="col-12 col-md-4">
-          <p>Cases in Review: {{clinicianStats.casesInProgress}}</p>
-          <p>Cases Sent to Practice: {{clinicianStats.casesWithAlert}}</p>
+          <p>Cases in Review: {{ statsCaseInReview }}</p>
+          <p>Cases Sent to Practice: {{ statsCaseInprogress }}</p>
         </div>
       </div>
     </div>
@@ -20,10 +20,7 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'ClinicianStats',
   computed: {
-    ...mapGetters(['clinicianStats'])
-  },
-  mounted () {
-    this.$store.dispatch('LOAD_CLINICIAN_STATS', 'c1')
+    ...mapGetters(['statsTotalCase', 'statsCaseCompleted', 'statsCaseInReview', 'statsCaseInprogress'])
   }
 }
 </script>
