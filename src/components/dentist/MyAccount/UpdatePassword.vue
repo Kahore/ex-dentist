552ec54a206925f88365262511b477542b26a475
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-30 19:05:16 +0600

    update password validation

--- src/components/dentist/MyAccount/UpdatePassword.vue
+++ src/components/dentist/MyAccount/UpdatePassword.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="col-lg-4 col-md-5 col-sm-12">
-      <form id="login-form" role="form" class="form-group ml-2">
+      <form id="updPassword-form" role="form" class="form-group ml-2">
         <h4 class="text-center">Update your password</h4>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.old_password.$error }">
           <input
             type="password"
             name="old_password"
@@ -12,8 +14,11 @@
             placeholder="Old Password"
             value=""
             v-model="old_password">
+          <div class="form-group__message--error" v-if="!$v.old_password.required">Old password is required.</div>
         </div>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.new_password.$error }">
           <input
             type="password"
             name="new_password"
@@ -23,8 +28,12 @@
             placeholder="New Password"
             value=""
             v-model="new_password">
+          <div class="form-group__message--error" v-if="!$v.new_password.required">New password is required.</div>
+          <div class="form-group__message--error" v-if="!$v.new_password.minLength">New password must have at least {{ $v.password.$params.minLength.min }} letters.</div>
         </div>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.confirm_password.$error }">
           <input
             type="password"
             name="confirm_password"
@@ -34,12 +43,13 @@
             placeholder="Confirm Password"
             value=""
             v-model="confirm_password">
+          <div class="form-group__message--error" v-if="!$v.confirm_password.sameAs">Passwords must be identical.</div>
         </div>
         <div class="form-group">
           <button
             class="btn btn-success"
             style="width: 100%"
-            @click.prevent="updatePassword">
+            @click.prevent="updatePassword()">
             Update Password
           </button>
         </div>
@@ -49,6 +59,7 @@
 </template>
 
 <script>
+import { required, sameAs, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'DentistUpdatePassword',
   data () {
@@ -58,9 +69,25 @@ export default {
       confirm_password: ''
     }
   },
+  validations: {
+    old_password: {
+      required,
+      minLength: minLength(6)
+    },
+    new_password: {
+      required,
+      minLength: minLength(6)
+    },
+    confirm_password: {
+      sameAs: sameAs('new_password')
+    }
+  },
   methods: {
     updatePassword () {
-      console.log('TCL: updatePassword -> updatePassword')
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        alert('Mock message for update password call')
+      }
     }
   }
 }
