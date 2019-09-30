f1cb1d03ec68956f24944b19e851204976c6e950
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-30 19:39:31 +0600

    Personal details validation

--- src/components/dentist/MyAccount/PersonalDetails.vue
+++ src/components/dentist/MyAccount/PersonalDetails.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="col-lg-4 col-md-5 offset-md-2 col-sm-12 offset-sm-1">
-      <form id="login-form" role="form" class="form-group ml-2">
+      <form id="personal-form" role="form" class="form-group ml-2">
         <h4 class="text-center">Personal Details</h4>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.currentUser.first_name.$error }">
           <input
             type="text"
             name="first_name"
@@ -12,8 +14,11 @@
             placeholder="First Name"
             value=""
             v-model="currentUser.first_name">
+          <div class="form-group__message--error" v-if="!$v.currentUser.first_name.required">First name is required.</div>
         </div>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.currentUser.last_name.$error }">
           <input
             type="text"
             name="last_name"
@@ -23,8 +28,11 @@
             placeholder="Last Name"
             value=""
             v-model="currentUser.last_name">
+          <div class="form-group__message--error" v-if="!$v.currentUser.last_name.required">Last name is required.</div>
         </div>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.currentUser.gdc_number.$error }">
           <input
             type="text"
             name="gdc_number"
@@ -34,24 +42,28 @@
             placeholder="GDC Name"
             value=""
             v-model="currentUser.gdc_number">
+            <div class="form-group__message--error" v-if="!$v.currentUser.gdc_number.required">GDC number is required.</div>
+            <div class="form-group__message--error" v-if="!$v.currentUser.gdc_number.numeric">GDC should be a numeric.</div>
         </div>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.currentUser.email.$error }">
           <input
             type="email"
             name="email"
-            id="email"
+            id="emailAdr"
             required
             class="form-control"
             placeholder="Email Address"
             value=""
             v-model="currentUser.email">
+            <div class="form-group__message--error" v-if="!$v.currentUser.email.required">Email is required.</div>
+            <div class="form-group__message--error" v-if="!$v.currentUser.email.email">Please, type a valid email.</div>
         </div>
         <div class="form-group">
           <button
-            class="btn btn-success"
-            style="width: 100%"
-            @click.prevent="updateDetails"
-            >
+            class="btn btn-success w-100"
+            @click.prevent="updateDentistDetails(currentUser)">
             Update Details
           </button>
         </div>
@@ -62,14 +74,41 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { required, email, numeric } from 'vuelidate/lib/validators'
 export default {
   name: 'DentistPersonalDetails',
+  data () {
+    return {
+      isLoading: false
+    }
+  },
+  validations: {
+    currentUser: {
+      first_name: {
+        required
+      },
+      last_name: {
+        required
+      },
+      gdc_number: {
+        required,
+        numeric
+      },
+      email: {
+        required,
+        email
+      }
+    }
+  },
   computed: {
     ...mapGetters(['currentUser'])
   },
   methods: {
-    updateDetails () {
-      console.log('TCL: updateDetails -> updateDetails')
+    updateDentistDetails (currentUser) {
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        alert('Mock message for update details call')
+      }
     }
   }
 }
