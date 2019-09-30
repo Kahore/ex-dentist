62f689f338dd1a412d1ed87b6184ae82d44d7cec
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-30 17:31:24 +0600

    Password recovery validation

--- src/components/auth/PasswordRecovery.vue
+++ src/components/auth/PasswordRecovery.vue
@@ -4,13 +4,17 @@
       <form id="login-form" role="form" class="form-group">
         <h3 class="text-center">Recovery password</h3>
         <p class="ml-2">Enter your registered email address, and we will send an email with the instructions on resetting your password.</p>
-        <div class="form-group">
+        <div
+          class="form-group"
+          :class="{ 'form-group--error': $v.email.$error }">
           <input
             type="email"
             name="email"
             id="email"
             class="form-control"
             placeholder="Email Address" v-model="email">
+          <div class="form-group__message--error" v-if="!$v.email.required">Email is required.</div>
+          <div class="form-group__message--error" v-if="!$v.email.email">Please, type a valid email.</div>
         </div>
         <div class="form-group">
           <button
@@ -31,11 +35,26 @@
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators'
 export default {
   name: 'PasswordRecovery',
+  data () {
+    return {
+      email: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    }
+  },
   methods: {
     resetPassword () {
-      console.log('TCL: resetPassword -> resetPassword')
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        alert('Mock message that password recovery was called ')
+      }
     }
   }
 }
